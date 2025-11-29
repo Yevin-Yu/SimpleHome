61328b1c4856675e02ef3a641088261094adcc36
Yevin Yu <yuwb0521@outlook.com>
2025-11-29 10:56:57 +0800

    [feat][v5.1.7][fonts]

--- src/main.ts
+++ src/main.ts
@@ -19,13 +19,4 @@ app.use(router);
 // 全局属性 是否是移动端
 app.provide("isMobile", isMobile());
 
-// 异步加载字体
-function loadCustomFont() {
-    const font = new FontFace('LXGW', 'url(fonts/LXGWWenKai-Medium.ttf) format("truetype")')
-    return font.load().then(loadedFace => document.fonts.add(loadedFace))
-}
-setTimeout(() => {
-    loadCustomFont()
-}, 1000)
-
 app.mount("#app");
